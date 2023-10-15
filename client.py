33be212647c6415af9f20eea76295af01a499130
Michael <tvq3bn@virginia.edu>
2023-10-15 18:53:09 -0400

    initialize the receive and send methods

--- client.py
+++ client.py
@@ -4,3 +4,18 @@ SERVER = socket.gethostbyname(socket.gethostname())
 PORT = 55556
 ADDRESS = (SERVER, PORT)
 FORMAT = "UTF-8"
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDRESS)
+
+"""
+Client should perform two things: receiving and sending messages to the server
+"""
+
+def receive():
+    pass
+
+
+
+def send():
+    pass
