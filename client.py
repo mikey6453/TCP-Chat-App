74e681e86c971e4a00114937732c90b2b1472d2c
Michael <tvq3bn@virginia.edu>
2023-10-15 19:00:10 -0400

    Implement receive method

--- client.py
+++ client.py
@@ -8,14 +8,23 @@ FORMAT = "UTF-8"
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDRESS)
 
+nickname = input("What is your nickname? ")
+
 """
 Client should perform two things: receiving and sending messages to the server
 """
 
+
 def receive():
-    pass
+    while True:
+        try:
+            message = client.recv(1024).decode(FORMAT)
+            if message == "NICK":
+                client.send(nickname.encode(FORMAT))
+            else:
+                print(message)
+        except:
+            print("An error occurred!")
+            client.close()
+            break
 
-
-
-def send():
-    pass
